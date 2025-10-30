f3cd5d53ecc185aeab1b7a6ba808c96417bcc4ce
Akash-Sharma-2002 <akashsharma21221@gmail.com>
2025-10-30 18:29:01 +0530

    About.jsx: created

--- cara-website/src/components/AboutUs/AboutApp.jsx
+++ cara-website/src/components/AboutUs/AboutApp.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import style from './AboutApp.module.css'
+import React from "react";
+import style from "./AboutApp.module.css";
+import aboutVideo from "../../assets/about/1.mp4";
 
 const AboutApp = () => {
   return (
-    <div>AboutApp</div>
-  )
-}
+    <div className={`${style.aboutApp} sectionp1`}>
+      <h1>
+        Download our <a href="#">App</a>
+      </h1>
+      <div className={style.video}>
+        <video autoplay muted loop src={aboutVideo}></video>
+      </div>
+    </div>
+  );
+};
 
-export default AboutApp
+export default AboutApp;
--- cara-website/src/components/AboutUs/AboutHead.jsx
+++ cara-website/src/components/AboutUs/AboutHead.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import style from './AboutApp.module.css'
+import React from "react";
+import style from "./AboutHead.module.css";
+import aboutImg from "../../assets/about/a6.jpg";
 
 const AboutHead = () => {
   return (
-    <div>AboutHead</div>
-  )
-}
+    <div className={`${style.aboutHead} sectionp1`}>
+      <img className={style.img} src={aboutImg} alt="" />
+      <div className={style.div}>
+        <h2>Who are we?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus,
+          officiis velit quisquam architecto incidunt voluptatum assumenda
+          cupiditate libero odit aperiam ut est, deserunt illum commodi dolores
+          placeat, perspiciatis vero pariatur? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Qui ad aperiam exercitationem distinctio
+          velit, commodi doloribus recusandae earum nihil, placeat, quod
+          praesentium labore dicta?
+        </p>
+        <br />
+        <abbr title="">
+          Create stunning image with as much aslittle control as you like thanks
+          to a choice of Basic and Creative modes.
+        </abbr>
+        <br />
+        <br />
+        <br />
+        <br />
+        <marquee loop="-1" scrollamount="5" width="100%" bgcolor="#ccc">
+          create stunning image with as much aslittle control as you like thanks
+          to a choice of Basic and Creative modes.
+        </marquee>
+      </div>
+    </div>
+  );
+};
 
-export default AboutHead
+export default AboutHead;
