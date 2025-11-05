e8ed03becfb4c226ff242e0fd9ee185fdc7b7441
Akash-Sharma-2002 <akashsharma21221@gmail.com>
2025-11-05 23:38:51 +0530

    Cart.jsx: initiated

--- cara-website/src/pages/Cart/Cart.jsx
+++ cara-website/src/pages/Cart/Cart.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import PageHeader from "../../components/PageHeader/PageHeader";
+import bGI from "../../../public/banner.png";
+import Footer from "../../components/Footer/Footer";
 
 const Cart = () => {
   return (
     <>
       <Navbar />
+      <PageHeader
+        h2="#Cart"
+        p="Add your fashion and stylings withot any limit.."
+        bGI={bGI}
+      />
+      <Footer />
     </>
   );
 };
